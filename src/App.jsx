a638141624d97e1e7f1307504b53bd4b61e99f2c
Kamoliddin Mirzaboyev <kamoliddinmirzaboyev05@gmail.com>
2025-02-13 12:17:56 +0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,70 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const imgs = ["/img1.svg", "/img2.svg", "/img3.svg"];
+  const [count, setCount] = useState(1);
+  const plus = () => {
+    setCount(count + 1);
+  };
+  const minus = () => {
+    setCount(count > 1 ? count - 1 : count);
+  };
+  const [orderImg, setOrderImg] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [mainImg, setMainImg] = useState(imgs[2]);
+  // Next img function
+  const nextImg = () => {
+    setOrderImg(orderImg < 2 ? orderImg + 1 : 0);
+    console.log(orderImg);
+  };
 
+  const firstActive = () => {
+    setOrderImg(0);
+    setIsActive(true);
+    setMainImg(imgs[orderImg]);
+  };
+  const secondActive = () => {
+    setOrderImg(1);
+    setIsActive(true);
+    setMainImg(imgs[orderImg]);
+  };
+  const thirdActive = () => {
+    setOrderImg(2);
+    setIsActive(true);
+    setMainImg(imgs[orderImg]);
+  };
+  const closeModal = () => {
+    setIsActive(false);
+  };
   return (
     <>
       <header>
+        <div className={isActive ? "modalBack modalActive" : "modalBack"}>
+          <div className="leftBtn">
+            <i className="fas fa-chevron-left"></i>
+          </div>
+          <div className="imgModal">
+            <img src={mainImg} alt="" />
+          </div>
+
+          <div
+            onClick={() => {
+              closeModal();
+            }}
+            className="closeModalBtn"
+          >
+            <i className="fa-solid fa-x"></i>
+          </div>
+
+          <div
+            onClick={() => {
+              nextImg();
+            }}
+            className="leftBtn"
+          >
+            <i className="fas fa-chevron-right"></i>
+          </div>
+        </div>
         <nav>
           <div className="container">
             <div className="logo">
@@ -53,9 +112,21 @@ function App() {
                 </p>
                 <div className="price">
                   <div className="counter">
-                    <button>-</button>
-                    <p>1</p>
-                    <button>+</button>
+                    <button
+                      onClick={() => {
+                        minus();
+                      }}
+                    >
+                      -
+                    </button>
+                    <p>{count}</p>
+                    <button
+                      onClick={() => {
+                        plus();
+                      }}
+                    >
+                      +
+                    </button>
                   </div>
                   <button className="addBag">Add to Bag</button>
                   <p>₹11,495.00</p>
@@ -66,19 +137,34 @@ function App() {
           <div className="heroProducts">
             <div className="container">
               <div className="mainProducts">
-                <div className="productImg">
-                  <img src="/img1.png" alt="" />
+                <div
+                  onClick={() => {
+                    firstActive();
+                  }}
+                  className="productImg"
+                >
+                  <img src="/img1.svg" alt="" />
                 </div>
-                <div className="productImg">
-                  <img src="/img2.png" alt="" />
+                <div
+                  onClick={() => {
+                    secondActive();
+                  }}
+                  className="productImg"
+                >
+                  <img src="/img2.svg" alt="" />
                 </div>
-                <div className="productImg">
-                  <img src="/img3.png" alt="" />
+                <div
+                  onClick={() => {
+                    thirdActive();
+                  }}
+                  className="productImg"
+                >
+                  <img src="/img3.svg" alt="" />
                 </div>
               </div>
               <div className="watchVideo">
                 <div className="videoIcon">
-                  <i class="fa-solid fa-play"></i>
+                  <i className="fa-solid fa-play"></i>
                 </div>
                 <p>Watch Video</p>
               </div>
